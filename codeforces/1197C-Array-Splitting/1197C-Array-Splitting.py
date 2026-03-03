532346716f63291b69704428f987d4593ae844ed
Abdellah <abdellahshafi7@gmail.com>
2026-03-03 11:17:00 +0300

    Add solution for Array Splitting

--- codeforces/1197C-Array-Splitting/1197C-Array-Splitting.py
+++ codeforces/1197C-Array-Splitting/1197C-Array-Splitting.py
@@ -1,31 +1,14 @@
 import sys
 data = list(map(int, sys.stdin.read().split()))
-n = data[0]
-k = data[1]
+n, k = data[0], data[1]
 nums = data[2:]
 
-if n == k:
-    sys.stdout.write("0")
-elif k == 1:
-    sys.stdout.write(str(nums[-1] - nums[0]))
+if k == 1:
+    print(nums[-1] - nums[0])
 else:
-    hashmap = {}
+    gaps = []
     for i in range(1, n):
-        gap = nums[i] - nums[i - 1]
-        hashmap[i] = gap
+        gaps.append(nums[i] - nums[i-1])
 
-    gaps = [i for i in range(1, len(nums))]
-    gaps.sort(reverse=True, key=lambda x: hashmap[x])
-
-    gaps = gaps[:k-1]
-
-    cost = 0
-    for i in range(len(gaps)):
-        if i == 0:
-            cost += (nums[-1] - nums[gaps[i]])
-        else:
-            cost += (nums[gaps[i - 1] - 1] - nums[gaps[i]])
-
-    cost += nums[gaps[-1] - 1] - nums[0]
-
-    sys.stdout.write(str(cost))
+    gaps.sort()
+    print(sum(gaps[:len(gaps)-(k-1)]))
