b53d91df91e5711235aaff4ff73359b82ee11e1f
Abdellah <abdellah.ugr-2822-16@aau.edu.et>
2026-03-11 21:53:23 +0300

    Add solution for Longest Subarray of 1's After Deleting One Element

--- leetcode/1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ leetcode/1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -2,35 +2,19 @@ class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         n = len(nums)
         max_ = 0
-        left = right = 0
-        zero_idx = 0
-        has_zero = False
 
-        while left < n and right < n:
-            right = left
-            seen = False
+        window = 0
+        left = 0
 
-            if nums[left] == 0:
-                zero_idx = left
-                has_zero = True
-                seen = True
+        for right in range(n):
+            window += nums[right]
 
-            while right + 1 < n and (nums[right + 1] == 1 or not seen):
-                right += 1
-
-                if nums[right] != 1:
-                    zero_idx = right
-                    seen = True
-                    has_zero = True
-            
-            window = right - left + (0 if seen else 1)
-            max_ = max(max_, window)
-
-            if zero_idx < left:
-                break
-            elif zero_idx == left:
+            while (right - left + 1) - window > 1:
+                window -= nums[left]
                 left += 1
-            else:
-                left = zero_idx + 1
 
-        return max_ + (0 if has_zero else -1)
+            max_ = max(max_, right - left)
+        
+        return max_
+
+
