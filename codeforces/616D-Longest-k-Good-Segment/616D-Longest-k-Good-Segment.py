dd501099cd87370f8ffd6f73d4151ded39992138
Abdellah <abdellah.ugr-2822-16@aau.edu.et>
2026-03-16 08:17:14 +0300

    Add solution for Longest k-Good Segment

--- codeforces/616D-Longest-k-Good-Segment/616D-Longest-k-Good-Segment.py
+++ codeforces/616D-Longest-k-Good-Segment/616D-Longest-k-Good-Segment.py
@@ -4,29 +4,26 @@ n, k = data[0], data[1]
 nums = data[2:n+2]
 
 left = 0
-count = 1
 max_ = 1
 max_left = 0
 hashmap = {nums[left]: 1}
 
 for right in range(1, n):
-    count += 1
     hashmap[nums[right]] = hashmap.get(nums[right], 0) + 1
 
-    while left < right and len(hashmap.keys()) > k:
-        count -= 1
+    while left < right and len(hashmap) > k:
         hashmap[nums[left]] = hashmap.get(nums[left], 0) - 1
         if hashmap[nums[left]] <= 0:
             del hashmap[nums[left]]
 
         left += 1
 
-    if count > max_ and len(hashmap.keys()) <= k:
-        max_ = count
+    if right - left + 1 > max_ and len(hashmap) <= k:
+        max_ = right - left + 1
         max_left = left
 
-if count > max_ and len(hashmap.keys()) <= k:
-    max_ = count
+if n - left > max_ and len(hashmap) <= k:
+    max_ = n - left
     max_left = left
 
 print(max_left + 1, max_left + max_)
