d6876a6c4220e238052ecb66727f1e4e8ba882c8
Abdellah <abdellah.ugr-2822-16@aau.edu.et>
2026-03-16 08:22:18 +0300

    Add solution for Binary Subarrays With Sum

--- leetcode/0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ leetcode/0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,29 +1,20 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
         n = len(nums)
-        
-        prefix = []
+        seen = {}
+        ans = 0
+
         total = 0
         for num in nums:
             total += num
-            prefix.append(total)
-        
-        ans = 0
+            diff = total - goal
 
-        hashmap = {}
-        for num in prefix:
-            if num == goal:
+            if diff == 0:
                 ans += 1
-                if 0 in hashmap:
-                    ans += hashmap[0]
-            elif num > goal:
-                target = num - goal
-                if target in hashmap:
-                    ans += hashmap[target]
             
-            hashmap[num] = hashmap.get(num, 0) + 1
-
-        if hashmap.get(0) == n and goal == 0:
-            return (n * (n + 1)) // 2
+            if diff in seen:
+                ans += seen[diff]
+            
+            seen[total] = seen.get(total, 0) + 1
 
         return ans
