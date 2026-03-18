f119df64cdbe4b8ecd1bd0646e76f4014e28b8cb
Abdellah <abdellah.ugr-2822-16@aau.edu.et>
2026-03-18 17:41:49 +0300

    Add solution for Design Browser History

---
+++ leetcode/1472-design-browser-history/1472-design-browser-history.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.head = Node(homepage)
+        self.tail = self.head
+
+    def visit(self, url: str) -> None:
+        new_node = Node(url)
+        self.tail.next = new_node
+        new_node.prev = self.tail
+
+        self.tail = new_node
+
+    def back(self, steps: int) -> str:
+        while self.tail.prev and steps != 0:
+            self.tail = self.tail.prev
+            steps -= 1
+        
+        return self.tail.val
+
+    def forward(self, steps: int) -> str:
+        while self.tail.next and steps != 0:
+            self.tail = self.tail.next
+            steps -= 1
+        
+        return self.tail.val
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
