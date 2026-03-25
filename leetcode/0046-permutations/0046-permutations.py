5c97932c04e7212329e01ae3fb0bfef4e71e2faa
Abdellah <abdellah.ugr-2822-16@aau.edu.et>
2026-03-25 11:48:18 +0300

    Add solution for Permutations

---
+++ leetcode/0046-permutations/0046-permutations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+            
+            for num in nums:
+                if num in path:
+                    continue
+                
+                path.append(num)
+                backtrack(path)
+                path.pop()
+        
+        backtrack([])
+        return res
