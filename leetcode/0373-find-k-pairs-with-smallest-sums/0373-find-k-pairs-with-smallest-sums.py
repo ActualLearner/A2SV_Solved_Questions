a311f58bfe233c1876338773c3d56dc8d95ec33a
Abdellah <abdellah.ugr-2822-16@aau.edu.et>
2026-05-18 20:49:36 +0300

    Add solution for Find K Pairs with Smallest Sums

---
+++ leetcode/0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -0,0 +1,21 @@
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        n, m = len(nums1), len(nums2)
+        ans = []
+        heap = [(nums1[0] + nums2[0], (0, 0))]
+        vis = set([(0, 0)])
+
+        while k > 0 and heap:
+            val, (i, j) = heappop(heap)
+            ans.append((nums1[i], nums2[j]))
+            k -= 1
+
+            if i + 1 < n and (i + 1, j) not in vis:
+                heappush(heap, (nums1[i + 1] + nums2[j], (i + 1, j)))
+                vis.add((i + 1, j))
+            
+            if j + 1 < m and (i, j + 1) not in vis:
+                heappush(heap, (nums1[i] + nums2[j + 1], (i, j + 1)))
+                vis.add((i, j + 1))
+
+        return ans
