542a8b909251595b605778bad56d68ef52a9f917
Abdellah <abdellah.ugr-2822-16@aau.edu.et>
2026-06-02 15:13:48 +0300

    Add solution for House Robber

--- leetcode/0198-house-robber/0198-house-robber.py
+++ leetcode/0198-house-robber/0198-house-robber.py
@@ -1,16 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        prev = nums[0]
+        prev1 = prev2 = 0
 
-        if n == 1:
-            return prev
-
-        curr = max(nums[0], nums[1])
-
-        for i in range(2, n):
-            temp = prev
-            prev = curr
-            curr = max(curr, temp + nums[i])
+        for num in nums:
+            curr = max(prev1 + num, prev2)
+            prev1 = prev2
+            prev2 = curr
         
-        return curr
+        return prev2
