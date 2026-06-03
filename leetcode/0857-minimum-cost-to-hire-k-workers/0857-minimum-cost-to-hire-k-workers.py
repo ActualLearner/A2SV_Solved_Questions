4ffc651ac00391c0aacb53881d21a7df35c72180
Abdellah <abdellah.ugr-2822-16@aau.edu.et>
2026-06-03 18:42:03 +0300

    Add solution for Minimum Cost to Hire K Workers

--- leetcode/0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
+++ leetcode/0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
@@ -9,20 +9,22 @@ class Solution:
         arr.sort()
         ans = float("inf")
         heap = []
+        sum_ = 0
         
         for i in range(n):
             if len(heap) < k - 1:
                 heappush(heap, -arr[i][2])
+                sum_ += arr[i][2]
                 continue
             
-            total = -sum(heap) + arr[i][2]
+            total = sum_ + arr[i][2]
             ans = min(ans, total * arr[i][0])
 
             if heap and arr[i][2] < -heap[0]:
-                heappop(heap)
+                temp = heappop(heap)
                 heappush(heap, -arr[i][2])
-        
-        
-        
+                sum_ += temp
+                sum_ += arr[i][2]
+
         return ans
             
