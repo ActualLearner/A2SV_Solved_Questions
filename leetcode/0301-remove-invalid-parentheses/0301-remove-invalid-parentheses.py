785bd1e071857a52e6d7f39f7e12c9d9a5298928
Abdellah <abdellah.ugr-2822-16@aau.edu.et>
2026-07-04 15:22:25 +0300

    Add solution for Remove Invalid Parentheses

--- leetcode/0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
+++ leetcode/0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
@@ -1,42 +1,45 @@
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
         n = len(s)
-        left_b, right_b = 0, 0
+        ans = set()
+        right = left = 0
 
         for char in s:
             if char == "(":
-                left_b += 1
+                right += 1
             elif char == ")":
-                if left_b > 0:
-                    left_b -= 1
-                else:
-                    right_b += 1
-
-
-        ans = set()
-        def bruteforce(path, i, left, right, count):
+                left += 1 if right == 0 else 0
+                right -= 1 if right > 0 else 0
+            
+        if right + left == 0:
+            return [s]
+        
+        def backtrack(i, right_b, left_b, path):
             if i == n:
-                if left + right + count == 0:
-                    ans.add("".join(path))
-                return 
+                if right_b + left_b != 0:
+                    return
 
-            if s[i] == "(" and left > 0:                    
-                bruteforce(path, i + 1, left - 1, right, count)
-            elif s[i] == ")" and right > 0:
-                bruteforce(path, i + 1, left, right - 1, count)
-            
-            if s[i] == "(":
-                count += 1
-            elif s[i] == ")":
-                count -= 1
-            
-            if count < 0:
+                right = left = 0
+                for char in path:
+                    if char == "(":
+                        right += 1
+                    elif char == ")":
+                        left += 1 if right == 0 else 0
+                        right -= 1 if right > 0 else 0
+
+                if left + right == 0:
+                    ans.add("".join(path))
+
                 return
-                
+
             path.append(s[i])
-            bruteforce(path, i + 1, left, right, count)
+            backtrack(i + 1, right_b, left_b, path)
             path.pop()
 
-
-        bruteforce([], 0, left_b, right_b, 0)
+            if right_b > 0 and s[i] == "(":
+                backtrack(i + 1, right_b - 1, left_b, path)
+            elif left_b > 0 and s[i] == ")":
+                backtrack(i + 1, right_b, left_b - 1, path)
+        
+        backtrack(0, right, left, [])
         return list(ans)
