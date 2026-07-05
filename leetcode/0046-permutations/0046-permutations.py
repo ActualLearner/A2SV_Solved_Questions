0a44efe238ff0d7533f7ea74cc39e55b6ce57afe
Abdellah <abdellah.ugr-2822-16@aau.edu.et>
2026-07-05 11:11:59 +0300

    Add solution for Permutations

--- leetcode/0046-permutations/0046-permutations.py
+++ leetcode/0046-permutations/0046-permutations.py
@@ -1,20 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        res = []
+        ans = []
 
-        def backtrack(path):
-            if len(path) == len(nums):
-                res.append(path[:])
-                return
+        def backtrack(i, path):
+            if i == n:
+                ans.append(path[:])
             
             for num in nums:
                 if num in path:
                     continue
                 
                 path.append(num)
-                backtrack(path)
+                backtrack(i + 1, path)
                 path.pop()
-        
-        backtrack([])
-        return res
+            
+        backtrack(0, [])
+        return ans
